6b13e9da41dc0b76612c933c93eb3b33de071473
claramattos <95841511+claramattos@users.noreply.github.com>
2024-11-14 23:18:19 +0100

    Add files via upload

--- access_api.py
+++ access_api.py
@@ -16,54 +16,101 @@ id = f"000078K"
 
 #Query
 #query = f"/api/v2/customer/profile/people/{id}"
-query = f"/api/v2/customer/social/group/"
-#query = f"/api/v2/customer/academic/member/{id}"
 
-# url complète avec l'autentication
-url = f"{base_url}{query}?access_id={config.key}&access_secret={config.secret}"
+query = "/api/v2/customer/profile/people"
 
-response = requests.get(url)
-if response.status_code == 200:
-    result = response.json() 
-    jprint(result)
-    print("Ok")
-else:
-    print(f"Erro: {response.status_code}")
+# url complète avec l'autentication (2460 pags)
+
+for pag in range (1, 246):
+    url = f"{base_url}{query}?access_id={config.key}&access_secret={config.secret}&page={pag}&limit=100"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        resultat = response.json() 
+        # jprint(result)
+        print("Ok")
+        '''
+        # ID de l'utilisateur (ID school, ID étudiant...)	
+        for i in range (len(resultat["_embedded"]["items"])):
+            result = resultat["_embedded"]["items"][i]
+            id_util = result["id"]
+            schoool_ref = result["school_ref"]
+
+            # Civilité	
+            civil = result["_embedded"]["civil"]["_embedded"]["civility"]["name"]
+
+            # Prénom	
+            prenom = result["_embedded"]["civil"]["firstname"]
+
+            # Nom d'usage ou marital
+            nom = result["_embedded"]["civil"]["lastname"]
+            nomUsage = result["_embedded"]["civil"]["name_used"]
+            if ( nomUsage == "lastname"):
+                nomUsage = nom
+                
+            # Nom d'état-civil	
+            nomNaissance = result["_embedded"]["civil"]["birthname"]
+
+            # Date de naissance (jj/mm/aaaa)	
+            dateNaissance = result["_embedded"]["civil"]["birthdate"]
+
+            # Nationalité	
+            nationalite = result["_embedded"]["civil"]["_embedded"]["nationality"]["name"]
+
+            # Lieu de naissance	-- pas trouvé
+
+            # addresse email personnelle	et étudiant
+            mails = result["_embedded"]["emails"]
+            for email in mails:
+                if email["type"] == "primary":
+                    mail_personnel = email        
+                elif email["type"] == "secondary":
+                    mail_etudiant = email
+
+            # Type d'utilisateur	
+            type = result["_embedded"]["type"]["name"]
+
+            # addresse - Ligne 1	
+            # addresse - Ligne 	
+            # addresse - Ligne 	
+            addresses = []
+            for item in result["_embedded"]["address"]:
+                if len(item["address"]) > 0:
+                    addresses.append(item)
+
+            # Code postal	
+            # Ville	
+            # État	
+            # Pays
+            # N'habite pas à l'addresse indiquée (NPAI)	
+            zips = []	
+            villes = []
+            etats = []
+            pays = []
+            for address in addresses:
+                if len(address["zip"]) != 0 and address["zip"] not in zips:
+                    zips.append(address["zip"])
+                if len(address["city"]) != 0 and address["city"] not in villes:
+                    villes.append(address["city"])
+                if len(address["region"]) != 0 and address["region"] not in etats:
+                    etats.append(address["region"])
+                if len(address["country_name"]) != 0 and address["country_name"] not in pays:
+                    pays.append(address["country_name"])
+                
+            # Genre   
+            gender = result["_embedded"]["civil"]["gender"]
+            if ( gender == "0"):
+                gender = "masculin"
+            else: 
+                gender = "feminin" 
+                '''
+    else:
+        print(f"Erro: {response.status_code}")
+
+
 
 '''
-#print(response.json()["display_name"])
-
-addresse = result["_embedded"]["address"]
-#schoolRef = 
-civil = result["_embedded"]["civil"]["_embedded"]["civility"]["name"]
-prenom = result["_embedded"]["civil"]["firstname"]
-nom = result["_embedded"]["civil"]["lastname"]
-nomNaissance = result["_embedded"]["civil"]["birthname"]
-dateNaissance = result["_embedded"]["civil"]["birthdate"]
-nationalite = result["_embedded"]["civil"]["_embedded"]["nationality"]["name"]
-
-gender = result["_embedded"]["civil"]["gender"]
-if ( gender == "0"):
-    gender = "masculin"
-else: 
-    gender = "feminin" 
-
-nomUsage = result["_embedded"]["civil"]["name_used"]
-if ( nomUsage == "lastname"):
-    nomUsage = nom
-
-#lieuNaissance = 
-emailPerso = result["_embedded"]["emails"][0]
-adresses = dict()
-adressePerso = {"AdresseLigne1" : "",
-                  "AddresseLigne2" : "",
-                 "AddresseLigne3" : "",
-                 "CodePostal" : 0,
-                 "Ville" : "",
-                 "Etat" : "",
-                 "Pays" : "",
-                 "NPAI" : 0} 
-#emailEtudiant = 
+# DIPLOME
 
 #typeDiplome = 
 #refDiplome = 
@@ -73,11 +120,10 @@ adressePerso = {"AdresseLigne1" : "",
 #ecole = 
 #paysEcole = 
 
-#listeAdresses = result["_embedded"]["address"]
-#for adresse in listeAdresses:
-#    print(adresse)
+#listeaddresses = result["_embedded"]["address"]
+#for addresse in listeaddresses:
+#    print(addresse)
 
-print(civil, prenom, nom, nomNaissance, dateNaissance, nationalite, gender, nomUsage, emailPerso, adressePerso)
 
 #Recuperer les infos du diplome
 query = f"/api/v2/customer/academic/member/{id}"
@@ -86,7 +132,15 @@ url = f"{base_url}{query}?access_id={config.key}&access_secret={config.secret}"
 response = requests.get(url)
 if response.status_code == 200:
     result = response.json() 
-    jprint(result)
+    #jprint(result)
     print("Ok")
+    # Type diplôme	
+    # Diplôme CNA	
+    # Programme AURION	
+    # Diplôme obtenu	
+    # Date intégration	
+    # Date probable obtention diplôme	
+    # Accord diffusion	
+    # Ecole
 else:
     print(f"Erro: {response.status_code}")'''
