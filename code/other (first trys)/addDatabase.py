ef9d38593c4c0a53dd55d407cbe036e8a1950bfa
Clara Mattos <mattosclara22@gmail.com>
2024-12-19 17:19:49 +0100

    organizing github with file deletion / updated MPD

--- code/other (first trys)/addDatabase.py
+++
@@ -1,6 +0,0 @@
-#Ajoute les donnees de resultat dans la database
-def addDatabase(resultat):
-    
-    getDiplomaInfo(resultat)
-    infosPersonnelles = getPersonneInfo(resultat)
-    databasePush(infosCarriere, infosPersonnelles)
